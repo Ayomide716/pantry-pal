2d6e83da66540b8cd3b9700986caaef7f17cede2
AYOMIDE FAMOYEGUN <ayomidefamoyegun1@gmail.com>
2025-09-13 14:33:43 +0000

    nothing is working

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    serverActions: {
+      allowedForwardedHosts: ['localhost', '127.0.0.1'],
+    },
+  },
 };
 
 export default nextConfig;
